08362556c57612706d7a965ecea5c14d58e1f77c
belenbouzon <belenbouzon@gmail.com>
2016-04-14 15:31:24 -0300

    Agrego contabilizador de paquetes
    PUEDE FALLAR

---
+++ cuantificacionTiposDePaquetes.py
@@ -0,0 +1,18 @@
+import sys
+import scapy.all
+
+
+if len(sys.argv)<1:
+	sys.exit("Contabilizando tipos de paquetes \n")
+
+packages = scapy.utils.rdpcap(sys.argv[1], int(sys.argv[2]))
+
+cants = dict()
+for packet in packages:
+	if packet.type in cants
+		cants[packet.type] = cants[packet.type] + 1
+	else 
+		cants[packet.type] = 1
+
+for key in cants.keys()
+	print " Type: " +  str(key) + "Amount :" + str(cants[key]) + "\n"
