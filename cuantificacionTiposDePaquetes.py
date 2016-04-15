82d565114d632fcd062b304617c777182292bd8c
Patricio Tarantino <patricio@cvam.com.ar>
2016-04-15 11:05:05 -0300

    Errores de sintaxis corregidos

--- cuantificacionTiposDePaquetes.py
+++ cuantificacionTiposDePaquetes.py
@@ -9,10 +9,10 @@ packages = scapy.utils.rdpcap(sys.argv[1], int(sys.argv[2]))
 
 cants = dict()
 for packet in packages:
-	if packet.type in cants
+	if packet.type in cants:
 		cants[packet.type] = cants[packet.type] + 1
-	else 
+	else:
 		cants[packet.type] = 1
 
-for key in cants.keys()
-	print " Type: " +  str(key) + "Amount :" + str(cants[key]) + "\n"
+for key in cants.keys():
+	print "Type: " +  str(key) + " - Amount: " + str(cants[key]) + "\n"
