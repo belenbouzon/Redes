e9624f3ce4f566f8638015507f7442d640d54d53
Previews <rey.maximiliano@gmail.com>
2016-04-20 13:34:49 -0300

    borrado de archivos viejos

--- escuchar.py
+++
@@ -1,6 +0,0 @@
-from scapy.all import *
-def monitor_callback(pkt):
-	print pkt.show()
-
-if __name__ == '__main__' :
-	sniff(prn=monitor_callback ,filter = "arp" , store = 0)
--- escuchar4.py
+++
@@ -1,6 +0,0 @@
-from scapy.all import *
-def monitor_callback(pkt):
-	print pkt.show()
-
-if __name__ == '__main__' :
-	sniff(prn=monitor_callback , store = 0)
